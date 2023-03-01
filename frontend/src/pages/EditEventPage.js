7ec1af1e045d721abc4b3c2686d5d87544ce3760
SammyTBrains <josephsamuel034@gmail.com>
2023-03-01 23:36:38 +0100

    check

---
+++ frontend/src/pages/EditEventPage.js
@@ -0,0 +1,5 @@
+const EditEventPage = () => {
+  return <h1>This is the Edit Event Page</h1>;
+};
+
+export default EditEventPage;
---
+++ frontend/src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <MainNavigation />
+      <h1>An Error occured!</h1>
+      <p>Couldn't load this page!</p>
+    </>
+  );
+};
+
+export default ErrorPage;
